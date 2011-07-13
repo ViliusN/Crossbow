d369843711ffb886aca528e686afa12bef46aeb8
Vilius Normantas <code@norma.lt>
2011-07-13 19:45:16 +0500

    Indicator Conditional created

---
+++ crossbow-core/src/indicators/Conditional.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2011 Vilius Normantas <code@norma.lt>
+ *
+ * This file is part of Crossbow library.
+ *
+ * Crossbow is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Crossbow is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Crossbow.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package lt.norma.crossbow.indicators
+
+import lt.norma.crossbow.core._
+
+/** Holds value of the specified indicator if `condition` results in `true` value. */
+class Conditional[Value : Manifest](indicator: Indicator[Value])(condition: Value => Boolean)
+    extends Indicator[Value] {
+  def name = "Conditional "+indicator.name
+  def dependencies = Set(indicator)
+
+  def calculate = {
+    case _ if(indicator.isEmpty) => None
+    case _ if(condition(indicator.value)) => indicator.value
+    case _ => None
+  }
+}
