18d2229232ba083a8c7542efe673c4efed0bdab4
Vilius Normantas <code@norma.lt>
2011-10-28 17:50:36 +0500

    Indicator Sqrt created

---
+++ crossbow-core/src/indicators/Sqrt.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010-2011 Vilius Normantas <code@norma.lt>
+ *
+ * This file is part of Crossbow library.
+ *
+ * Crossbow is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Crossbow is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Crossbow.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package lt.norma.crossbow.indicators
+
+import lt.norma.crossbow.core._
+
+/** Calculates square root of the specified target indicator. */
+class Sqrt(target: Indicator[Double]) extends Indicator[Double] {
+  def name = "Sqrt("+target.name+")"
+  def dependencies = Set(target)
+  def calculate = {
+    case _ if(target.isSet && target.value >= 0) => math.sqrt(target.value)
+    case _ => None
+  }
+}
